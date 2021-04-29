cf7a8996862022a79219f92686210ecb94ecfe82
Bartosz Kaszubowski <gosimek@gmail.com>
2021-04-29 13:57:51 +0200

    improve code structure, code cleanup, update Readme

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 # Yarn Lock Changes
 
-Creates a comment inside Pull Request with the human-readable summary of the changes to the `yarn.lock` file.
-
-## Preview
-
-### Short list of changes
-
-<img alt="Screenshot 2021-04-28 120655" src="https://user-images.githubusercontent.com/719641/116386618-3f53ba80-a81a-11eb-9438-6601d401f880.png">
-
-### Long list of changes
-
-<img alt="Screenshot 2021-04-28 115019" src="https://user-images.githubusercontent.com/719641/116385385-09620680-a819-11eb-857c-113f9048d856.png">
+Creates a comment inside Pull Request with the human-readable summary of the changes to the `yarn.lock` file. Works in public and private repositories, offers a degree of customization.
 
 ## Usage
 
@@ -31,3 +21,13 @@ Creates a comment inside Pull Request with the human-readable summary of the cha
 | `path` | No | `'yarn.lock'` | Path to the `yarn.lock` file in the repository. Default value points to the file at project root. |
 | `token` | **Yes** | - | GitHub token for the bot, so it can publish a comment in the pull request. |
 | `updateComment` | No | `'true'` | Should the bot update the summary comment. If value is `'false'`, bot will post a new comment on each new commit. |
+
+## Preview
+
+### Short list of changes
+
+<img alt="Screenshot 2021-04-28 120655" src="https://user-images.githubusercontent.com/719641/116386618-3f53ba80-a81a-11eb-9438-6601d401f880.png">
+
+### Long list of changes
+
+<img alt="Screenshot 2021-04-28 115019" src="https://user-images.githubusercontent.com/719641/116385385-09620680-a819-11eb-857c-113f9048d856.png">
---
+++ src/action.js
@@ -0,0 +1,125 @@
+const { getInput, setFailed } = require('@actions/core');
+const { context, getOctokit } = require('@actions/github');
+const lockfile = require('@yarnpkg/lockfile');
+const fs = require('fs');
+const { Base64 } = require('js-base64');
+const path = require('path');
+
+const { createTable, createSummary, diffLocks } = require('./utils');
+
+const COMMENT_HEADER = '## `yarn.lock` changes';
+
+const getBooleanInput = (input) => {
+  const trueValues = ['true', 'yes', 'y', 'on'];
+  const falseValues = ['false', 'no', 'n', 'off'];
+  const inputValue = getInput(input).toLowerCase();
+
+  if (trueValues.includes(inputValue)) {
+    return true;
+  } else if (falseValues.includes(inputValue)) {
+    return false;
+  }
+
+  throw TypeError(`💥 Wrong boolean value of the input '${input}', aborting!`);
+};
+
+const run = async () => {
+  try {
+    const octokit = getOctokit(getInput('token', { required: true }));
+    const inputPath = getInput('path');
+    const updateComment = getBooleanInput('updateComment');
+    const collapsibleThreshold = Math.max(parseInt(getInput('collapsibleThreshold'), 10), 0);
+
+    const { owner, repo, number } = context.issue;
+    const oktokitParams = { owner, repo };
+
+    if (!number) {
+      throw Error('💥 Cannot find the PR, aborting!');
+    }
+
+    const lockPath = path.resolve(process.cwd(), inputPath);
+
+    if (!fs.existsSync(lockPath)) {
+      throw Error('💥 It looks like lock does not exist in this PR, aborting!');
+    }
+
+    const content = await fs.readFileSync(lockPath, { encoding: 'utf8' });
+    const updatedLock = lockfile.parse(content);
+
+    const masterLockResponse = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      ...oktokitParams,
+      path: inputPath
+    });
+
+    if (!masterLockResponse || !masterLockResponse.data || !masterLockResponse.data.content) {
+      throw Error('💥 Cannot fetch base lock, aborting!');
+    }
+
+    const masterLock = lockfile.parse(Base64.decode(masterLockResponse.data.content));
+    const lockChanges = diffLocks(masterLock, updatedLock);
+    const lockChangesCount = Object.keys(lockChanges).length;
+
+    if (lockChangesCount) {
+      const diffsTable = createTable(lockChanges);
+      const collapsed = lockChangesCount >= collapsibleThreshold;
+
+      const changesSummary = collapsed ? '### Summary\n' + createSummary(lockChanges) : '';
+
+      const commentBody =
+        COMMENT_HEADER +
+        '\n' +
+        changesSummary +
+        '\n' +
+        '<details' +
+        (collapsed ? '' : ' open') +
+        '>\n' +
+        '<summary>Click to toggle table visibility</summary>\n<br/>\n\n' +
+        diffsTable +
+        '\n\n' +
+        '</details>';
+
+      if (updateComment) {
+        const currentComments = await octokit.issues.listComments({
+          ...oktokitParams,
+          issue_number: number,
+          per_page: 100
+        });
+
+        if (!currentComments || !currentComments.data) {
+          throw Error('💥 Cannot fetch PR comments, aborting!');
+        }
+
+        const commentId = currentComments.data
+          .filter(
+            ({ user, body }) =>
+              user.login === 'github-actions[bot]' && body.startsWith(COMMENT_HEADER)
+          )
+          .map(({ id }) => id)[0];
+
+        if (commentId) {
+          await octokit.issues.updateComment({
+            ...oktokitParams,
+            comment_id: commentId,
+            body: commentBody
+          });
+        } else {
+          await octokit.issues.createComment({
+            ...oktokitParams,
+            issue_number: number,
+            body: commentBody
+          });
+        }
+      } else {
+        await octokit.issues.createComment({
+          ...oktokitParams,
+          issue_number: number,
+          body: commentBody
+        });
+      }
+    }
+  } catch (error) {
+    setFailed(error.message);
+  }
+};
+
+run();
