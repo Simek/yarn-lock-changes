34017425198654c20162a4dfd4f238fbece9636f
Bartosz Kaszubowski <gosimek@gmail.com>
2025-05-04 21:02:33 +0200

    improve parser performance, add berry parser to perf test (#85)

--- tests/performance/parse.test.mjs
+++ tests/performance/parse.test.mjs
@@ -1,31 +1,67 @@
 import lockfile from '@yarnpkg/lockfile';
+import { parseSyml } from '@yarnpkg/parsers';
 import { test } from 'uvu';
 import { is } from 'uvu/assert';
 
 import { getTestLockContent } from '../testUtils.mjs';
 import { parseLock } from '../../src/utils.mjs';
 
-test('naive performance test', () => {
+test('Classic - parser performance test (x100)', () => {
   const contentA = getTestLockContent('classic-downgrade', 'a.lock');
   const contentB = getTestLockContent('classic-downgrade', 'b.lock');
 
-  console.time('@yarnpkg/lockfile');
+  console.log('\nClassic - parser performance test (x100)');
+  console.time('@yarnpkg/lockfile\t');
   const start = performance.now();
 
-  lockfile.parse(contentA);
-  lockfile.parse(contentB);
+  for (let i = 0; i < 100; i++) {
+    lockfile.parse(contentA);
+    lockfile.parse(contentB);
+  }
 
   const end = performance.now();
-  console.timeEnd('@yarnpkg/lockfile');
+  console.timeEnd('@yarnpkg/lockfile\t');
 
-  console.time('Internal parser');
+  console.time('Internal\t\t');
   const internalStart = performance.now();
 
-  parseLock(contentA);
-  parseLock(contentB);
+  for (let i = 0; i < 100; i++) {
+    parseLock(contentA);
+    parseLock(contentB);
+  }
 
   const internalEnd = performance.now();
-  console.timeEnd('Internal parser');
+  console.timeEnd('Internal\t\t');
+
+  is(end - start > internalEnd - internalStart, true);
+});
+
+test('Berry - parser performance test (x100)', () => {
+  const contentA = getTestLockContent('berry-v3', 'a.lock');
+  const contentB = getTestLockContent('berry-v3', 'b.lock');
+
+  console.log('\nBerry - parser performance test (x100)');
+  console.time('@yarnpkg/parsers\t');
+  const start = performance.now();
+
+  for (let i = 0; i < 100; i++) {
+    parseSyml(contentA);
+    parseSyml(contentB);
+  }
+
+  const end = performance.now();
+  console.timeEnd('@yarnpkg/parsers\t');
+
+  console.time('Internal\t\t');
+  const internalStart = performance.now();
+
+  for (let i = 0; i < 100; i++) {
+    parseLock(contentA);
+    parseLock(contentB);
+  }
+
+  const internalEnd = performance.now();
+  console.timeEnd('Internal\t\t');
 
   is(end - start > internalEnd - internalStart, true);
 });
--- tests/testUtils.mjs
+++ tests/testUtils.mjs
@@ -1,5 +1,6 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
+import process from 'node:process';
 
 export const getTestLockContent = (testName, filename) => {
   return fs.readFileSync(path.resolve(process.cwd(), './tests/unit/', testName, filename), {
